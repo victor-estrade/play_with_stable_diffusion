27a9ba9d4c211c7d2ceaac1421278142719868d6
Victor Estrade <victor.antoine.estrade@gmail.com>
2022-09-13 19:33:19 +0200

    Fix : 'CLIPTokenizer' object has no attribute 'to'

--- stable_diffusion/factory.py
+++ stable_diffusion/factory.py
@@ -46,7 +46,7 @@ class Factory:
         self.enable_attention_slicing = enable_attention_slicing
         self.vae = self._make_vae(model_checkpoint, half_precision, auth_token).to(device)
         self.text_encoder = self._make_text_encoder(model_checkpoint, half_precision, auth_token).to(device)
-        self.tokenizer = self._make_tokenizer(model_checkpoint, half_precision, auth_token).to(device)
+        self.tokenizer = self._make_tokenizer(model_checkpoint, half_precision, auth_token)
         self.unet = self._make_unet(model_checkpoint, half_precision, auth_token).to(device)
         self.scheduler = self._make_scheduler(model_checkpoint, half_precision, auth_token).to(device)
         self.safety_checker = self._make_safety_checker(model_checkpoint, half_precision, censored, auth_token).to(device)
