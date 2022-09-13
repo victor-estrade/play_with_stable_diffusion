4d024b0f5b3795797819bc891e0484416041e8a1
Victor Estrade <victor.antoine.estrade@gmail.com>
2022-09-13 10:21:02 +0200

    typo

--- stable_diffusion/factory.py
+++ stable_diffusion/factory.py
@@ -24,7 +24,7 @@ from .utils import PIL_IMAGE_RESAMPLING
 
 class Factory:
     def __init__(self, 
-        model_chekpoint="CompVis/stable-diffusion-v1-4", 
+        model_checkpoint="CompVis/stable-diffusion-v1-4", 
         device="cuda",
         half_precision=True,
         enable_attention_slicing=False,
@@ -41,101 +41,101 @@ class Factory:
         :param auth_token: the HuggingFace Token since downloading these models requires to agree with its Licence.
 
         """
-        self.model_chekpoint = model_chekpoint
+        self.model_checkpoint = model_checkpoint
         self.device = device
         self.half_precision = half_precision
         self.enable_attention_slicing = enable_attention_slicing
-        self.vae = self._make_vae(model_chekpoint, half_precision, auth_token)
-        self.text_encoder = self._make_text_encoder(model_chekpoint, half_precision, auth_token)
-        self.tokenizer = self._make_tokenizer(model_chekpoint, half_precision, auth_token)
-        self.unet = self._make_unet(model_chekpoint, half_precision, auth_token)
-        self.scheduler = self._make_scheduler(model_chekpoint, half_precision, auth_token)
-        self.safety_checker = self._make_safety_checker(model_chekpoint, half_precision, auth_token)
-        self.feature_extractor = self._make_feature_extractor(model_chekpoint, half_precision, auth_token)
+        self.vae = self._make_vae(model_checkpoint, half_precision, auth_token)
+        self.text_encoder = self._make_text_encoder(model_checkpoint, half_precision, auth_token)
+        self.tokenizer = self._make_tokenizer(model_checkpoint, half_precision, auth_token)
+        self.unet = self._make_unet(model_checkpoint, half_precision, auth_token)
+        self.scheduler = self._make_scheduler(model_checkpoint, half_precision, auth_token)
+        self.safety_checker = self._make_safety_checker(model_checkpoint, half_precision, auth_token)
+        self.feature_extractor = self._make_feature_extractor(model_checkpoint, half_precision, auth_token)
 
     # ================
     # Inner factory to load the models
     # ================
-    def _make_vae(self, model_chekpoint, half_precision, auth_token):
+    def _make_vae(self, model_checkpoint, half_precision, auth_token):
         if half_precision:
             vae = AutoencoderKL.from_pretrained(
-                model_chekpoint,
+                model_checkpoint,
                 subfolder="vae",
                 revision="fp16",
                 torch_dtype=torch.float16,
                 use_auth_token=auth_token)
         else:
             vae = AutoencoderKL.from_pretrained(
-                model_chekpoint,
+                model_checkpoint,
                 subfolder="vae",
                 use_auth_token=auth_token)
         return vae
 
-    def _make_text_encoder(self, model_chekpoint, half_precision, auth_token):
+    def _make_text_encoder(self, model_checkpoint, half_precision, auth_token):
         if half_precision:
             text_encoder = CLIPTextModel.from_pretrained(
-                model_chekpoint,
+                model_checkpoint,
                 subfolder="text_encoder",
                 revision="fp16",
                 torch_dtype=torch.float16,
                 use_auth_token=auth_token)
         else:
             text_encoder = CLIPTextModel.from_pretrained(
-                model_chekpoint,
+                model_checkpoint,
                 subfolder="text_encoder",
                 use_auth_token=auth_token)
         return text_encoder
 
-    def _make_tokenizer(self, model_chekpoint, half_precision, auth_token):
+    def _make_tokenizer(self, model_checkpoint, half_precision, auth_token):
         if half_precision:
             tokenizer = CLIPTokenizer.from_pretrained(
-                model_chekpoint,
+                model_checkpoint,
                 subfolder="tokenizer",
                 revision="fp16",
                 torch_dtype=torch.float16,
                 use_auth_token=auth_token)
         else:
             tokenizer = CLIPTokenizer.from_pretrained(
-                model_chekpoint,
+                model_checkpoint,
                 subfolder="tokenizer",
                 use_auth_token=auth_token)
         return tokenizer
 
-    def _make_unet(self, model_chekpoint, half_precision, auth_token):
+    def _make_unet(self, model_checkpoint, half_precision, auth_token):
         if half_precision:
             unet = UNet2DConditionModel.from_pretrained(
-                model_chekpoint,
+                model_checkpoint,
                 subfolder="unet",
                 revision="fp16",
                 torch_dtype=torch.float16,
                 use_auth_token=auth_token)
         else:
             unet = UNet2DConditionModel.from_pretrained(
-                model_chekpoint,
+                model_checkpoint,
                 subfolder="unet",
                 use_auth_token=auth_token)
         return unet
 
-    def _make_scheduler(self, model_chekpoint, half_precision, auth_token):
+    def _make_scheduler(self, model_checkpoint, half_precision, auth_token):
         scheduler = PNDMScheduler(beta_start=0.00085, beta_end=0.012, beta_schedule="scaled_linear", num_train_timesteps=1000)
         return scheduler
 
-    def _make_safety_checker(self, model_chekpoint, half_precision, auth_token):
+    def _make_safety_checker(self, model_checkpoint, half_precision, auth_token):
         if half_precision:
             safety_checker = StableDiffusionSafetyChecker.from_pretrained(
-                model_chekpoint,
+                model_checkpoint,
                 subfolder="safety_checker",
                 revision="fp16",
                 torch_dtype=torch.float16,
                 use_auth_token=auth_token)
         else:
             safety_checker = StableDiffusionSafetyChecker.from_pretrained(
-                model_chekpoint,
+                model_checkpoint,
                 subfolder="safety_checker",
                 use_auth_token=auth_token)
         return safety_checker
 
-    def _make_feature_extractor(self, model_chekpoint, half_precision, auth_token):
+    def _make_feature_extractor(self, model_checkpoint, half_precision, auth_token):
         feature_extractor = CLIPFeatureExtractor(
             crop_size = 224,
             do_center_crop = True,
