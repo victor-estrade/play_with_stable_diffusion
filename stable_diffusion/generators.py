0d19b2d8834619c305454cc524535c368d002078
Victor Estrade <victor.antoine.estrade@gmail.com>
2022-09-13 18:56:51 +0200

    Add Image to image pipeline and app

--- stable_diffusion/generators.py
+++ stable_diffusion/generators.py
@@ -1,52 +1,11 @@
 import logging
 import torch
 
+from PIL import Image
+
 from diffusers.pipelines.stable_diffusion import StableDiffusionPipeline, StableDiffusionImg2ImgPipeline, StableDiffusionInpaintPipeline
 
 logger = logging.getLogger(__name__)
-
-
-def generate_from_prompt(
-    pipe,
-    prompt:str,
-    width:int=512,
-    height:int=512,
-    num_images=4,
-    num_inference_steps=50,
-    prompt_strength:float=1.0,
-    guidance_scale=7.5,
-    seed=None,
-    ):
-    """
-    :param pipe: the diffusion model
-    :param prompt: the sentence prompt to generate the images
-    :param num_images: the number of images to degenerate from the prompt
-    :param num_inference_steps: the number of inference step. The more steps the better the image quality but also slower
-    :param guidance_scale: the guidance scale (see guided diffusion tricks)
-    :param seed: (default None) the seed for the random generator. Use None to get new seed for each run.
-
-    """
-    generator = torch.Generator(pipe.device).manual_seed(seed) if seed else None
-
-    with torch.autocast("cuda"):
-        results = [
-            pipe(
-                prompt=prompt,
-                width=width,
-                height=height,
-                mask=None,
-                init_image=None,
-                num_inference_steps=num_inference_steps,
-                prompt_strength=prompt_strength,
-                guidance_scale=guidance_scale,
-                generator=generator
-                )
-            for i in range(num_images)]
-
-    images = [image for result in results for image in result["sample"]]
-    nsfw_content_detected = [is_nsfw for result in results for is_nsfw in result["nsfw_content_detected"]]
-    return images, nsfw_content_detected
-
 
 
 class TextToImageGenerator():
@@ -60,7 +19,6 @@ class TextToImageGenerator():
         height:int=512,
         num_images=4,
         num_inference_steps=50,
-        prompt_strength:float=1.0,
         guidance_scale=7.5,
         seed=None,
         ):
@@ -71,7 +29,6 @@ class TextToImageGenerator():
         :param height: the height of the images
         :param num_images: the number of images to degenerate from the prompt
         :param num_inference_steps: the number of inference step. The more steps the better the image quality but also slower
-        :param prompt_strength: the strength of the prompt (leave it to 1.0).
         :param guidance_scale: the guidance scale (see guided diffusion tricks)
         :param seed: (default None) the seed for the random generator. Use None or 0 to get new seed for each run.
 
@@ -90,7 +47,77 @@ class TextToImageGenerator():
                     mask=None,
                     init_image=None,
                     num_inference_steps=num_inference_steps,
-                    prompt_strength=prompt_strength,
+                    guidance_scale=guidance_scale,
+                    generator=generator
+                    )
+                for i in range(num_images)]
+
+        images = [image for result in results for image in result["sample"]]
+        nsfw_content_detected = [is_nsfw for result in results for is_nsfw in result["nsfw_content_detected"]]
+        return images, nsfw_content_detected
+
+
+
+class ImageToImageGenerator():
+    def __init__(self, pipe : StableDiffusionImg2ImgPipeline) -> None:
+        self.pipe = pipe
+
+    def __call__(
+        self,
+        prompt:str,
+        init_image : Image,
+        width:int=512,
+        height:int=512,
+        num_images=4,
+        num_inference_steps=50,
+        strength:float=1.0,
+        guidance_scale=7.5,
+        seed=None,
+        ):
+        """ Generate images from given text prompt.
+
+        :param prompt: 
+            the sentence prompt to generate the images
+        :param init_image:
+            the initial image
+        :param width:
+            the width of the images
+        :param height:
+            the height of the images
+        :param num_images:
+            the number of images to degenerate from the prompt
+        :param num_inference_steps:
+            the number of inference step. The more steps the better the image quality but also slower
+        :param strength:
+            Conceptually, indicates how much to transform the reference `init_image`. Must be between 0 and 1.
+            `init_image` will be used as a starting point, adding more noise to it the larger the `strength`. The
+            number of denoising steps depends on the amount of noise initially added. When `strength` is 1, added
+            noise will be maximum and the denoising process will run for the full number of iterations specified in
+            `num_inference_steps`. A value of 1, therefore, essentially ignores `init_image`.
+        :param guidance_scale:
+            the guidance scale (see guided diffusion tricks)
+        :param seed: (default None)
+            the seed for the random generator. Use None or 0 to get new seed for each run.
+
+        :return:
+            images :
+                the list of PIL Images
+            nsfw_content_detected :
+                the list of flag if the content is Not Safe For Work
+        """
+        generator = torch.Generator(self.pipe.device).manual_seed(seed) if seed else None
+
+        with torch.autocast("cuda"):
+            results = [
+                self.pipe(
+                    prompt=prompt,
+                    init_image=init_image,
+                    width=width,
+                    height=height,
+                    mask=None,
+                    init_image=None,
+                    num_inference_steps=num_inference_steps,
+                    strength=strength,
                     guidance_scale=guidance_scale,
                     generator=generator
                     )
--- stable_diffusion/webapp.py
+++ stable_diffusion/webapp.py
@@ -8,7 +8,7 @@ import gradio as gr
 
 import numpy as np
 
-from .generators import TextToImageGenerator
+from .generators import TextToImageGenerator, ImageToImageGenerator
 from .factory import Factory
 from .images import auto_glue_image_grid
 
@@ -22,7 +22,7 @@ class TextToImageBuilder():
         max_num_images:int=9,
         images_per_row:int=3,
         ):
-        """ App builder
+        """ App builder for Text to Image pipeline
         """
         self.text_to_image_generator = text_to_image_generator
         self.max_num_images = max_num_images
@@ -114,6 +114,108 @@ class TextToImageBuilder():
 
 
 
+
+class ImageToImageBuilder():
+    def __init__(self,
+        text_to_image_generator : ImageToImageGenerator,
+        max_num_images:int=9,
+        images_per_row:int=3,
+        ):
+        """ App builder for Image to Image pipeline
+        """
+        self.text_to_image_generator = text_to_image_generator
+        self.max_num_images = max_num_images
+        self.images_per_row = images_per_row
+    
+    @classmethod
+    def from_factory(cls,
+        factory : Factory,
+        max_num_images:int=9,
+        images_per_row:int=3,
+        device="cuda",
+        ):
+        """ Alternative constructor using a Factory
+        """
+        generator = ImageToImageGenerator(factory.make_image_to_image_pipeline().to(device))
+        new_builder = cls(generator, max_num_images=max_num_images, images_per_row=images_per_row)
+        return new_builder
+
+
+    def __call__(
+        self,
+        prompt,
+        init_image,
+        num_image,
+        width,
+        height,
+        seed,
+        guidance_scale,
+        num_inference_steps,
+        strength,
+        ):
+        """ Wrapper around the generator.
+        It is used as the callable function in gradio's elements to generate the image.
+        """
+        images, nsfw_content_detected = self.text_to_image_generator(
+            prompt=prompt,
+            init_image=init_image,
+            width=width,
+            height=height,
+            num_images=num_image,
+            num_inference_steps=num_inference_steps,
+            strength=strength,
+            guidance_scale=guidance_scale,
+            seed=seed,
+        )
+        print(nsfw_content_detected)
+
+        images_out = [auto_glue_image_grid(images)] + images + [None] * (self.max_num_images - len(images))
+
+        return images_out
+
+
+    def build(self):
+        prompt = gr.Textbox(label="prompt")
+        init_image = gr.Image(label="Initial image")
+        with gr.Row():
+            width = gr.Slider(minimum=64, maximum=1024, value=512, step=64, label="width")
+            height = gr.Slider(minimum=64, maximum=1024, value=512, step=64, label="height")
+        with gr.Row():
+            seed = gr.Number(0, label="seed : 0 = no seed")
+            guidance_scale = gr.Number(7.5, label="guidance_scale")
+        num_inference_steps = gr.Slider(minimum=5, maximum=200, value=50, step=5, label="num_inference_steps")
+        prompt_strength = gr.Slider(minimum=0., maximum=1.0, value=1.0, step=0.05, label="prompt_strength")
+        num_images = gr.Slider(minimum=1, maximum=self.max_num_images, value=default.NUM_IMAGES, step=1, label="Number of images")
+
+        inputs = [
+            prompt, init_image, num_images,
+            width, height,
+            seed,
+            guidance_scale,
+            num_inference_steps,
+            prompt_strength,
+            ]
+
+        greet_btn = gr.Button("Generate !")
+
+        out_glued_image = gr.Image(label=f"Generated images")
+
+        with gr.Accordion("Generated individual images"):
+            i = 0
+            output_images = []
+            while i < self.max_num_images:
+                with gr.Row():
+                    remaining_out_images_to_init = self.max_num_images - i
+                    for j in range(min(self.images_per_row, remaining_out_images_to_init)):
+                        out_img = gr.Image(label=f"Generated {i+1}")
+                        output_images.append(out_img)
+                        i += 1
+        outputs = [out_glued_image] + output_images
+        greet_btn.click(fn=self, inputs=inputs, outputs=outputs)
+
+
+
+
 @click.command()
 @click.option('--share', is_flag=True, show_default=True, default=False,
     help="Share the app. Create a proxy in gradio's website to access the app.")
@@ -123,18 +225,31 @@ def main(share, nsfw):
     """ Main function to run the gradio web app.
     """
 
-    logger.info("Loading the models ...")
-    FACTORY = Factory(censored=not nsfw)
-    TEXT_TO_IMAGE_APP_BUILDER = TextToImageBuilder.from_factory(FACTORY)
-    logger.info("Models are ready")
-
     MAX_NUM_IMAGES = 9
     IMAGES_PER_ROW = 3
+
+    logger.info("Loading the models ...")
+    FACTORY = Factory(censored=not nsfw)
+    TEXT_TO_IMAGE_APP_BUILDER = TextToImageBuilder.from_factory(
+        FACTORY,
+        max_num_images=MAX_NUM_IMAGES,
+        images_per_row=IMAGES_PER_ROW,
+        )
+    IMAGE_TO_IMAGE_APP_BUILDER = ImageToImageBuilder.from_factory(
+        FACTORY,
+        max_num_images=MAX_NUM_IMAGES,
+        images_per_row=IMAGES_PER_ROW,
+        )
+    logger.info("Models are ready")
+
 
     with gr.Blocks() as demo:
         with gr.Tab("Text to image pipeline") :
             TEXT_TO_IMAGE_APP_BUILDER.build()
+        with gr.Tab("Image to image pipeline") :
+            IMAGE_TO_IMAGE_APP_BUILDER.build()
     demo.launch(share=share)
+
 
 if __name__ == "__main__":
     main()
