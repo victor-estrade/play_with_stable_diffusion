44e37c151391ffc2d5fc500845c62f6ded0e2185
Victor Estrade <victor.antoine.estrade@gmail.com>
2022-09-29 16:03:06 +0200

    Add sketch to image app

--- stable_diffusion/webapp.py
+++ stable_diffusion/webapp.py
@@ -215,6 +215,103 @@ class ImageToImageBuilder:
         generate_btn.click(fn=self, inputs=inputs, outputs=outputs)
 
 
+
+class SketchToImageBuilder:
+    def __init__(
+        self,
+        text_to_image_generator: ImageToImageGenerator,
+        max_num_images: int = 9,
+        images_per_row: int = 3,
+    ):
+        """App builder for Image to Image pipeline"""
+        self.text_to_image_generator = text_to_image_generator
+        self.max_num_images = max_num_images
+        self.images_per_row = images_per_row
+        self.gallery_grid = [
+            max(1, self.images_per_row - 2),
+            max(1, self.images_per_row - 1),
+            max(1, self.images_per_row),
+        ]
+
+    @classmethod
+    def from_factory(
+        cls,
+        factory: Factory,
+        max_num_images: int = 9,
+        images_per_row: int = 3,
+    ):
+        """Alternative constructor using a Factory"""
+        generator = ImageToImageGenerator(factory.make_image_to_image_pipeline())
+        new_builder = cls(
+            generator, max_num_images=max_num_images, images_per_row=images_per_row
+        )
+        return new_builder
+
+    def __call__(
+        self,
+        prompt,
+        init_image,
+        num_image,
+        seed,
+        guidance_scale,
+        num_inference_steps,
+        strength,
+    ):
+        """Wrapper around the generator.
+        It is used as the callable function in gradio's elements to generate the image.
+        """
+        images, nsfw_content_detected = self.text_to_image_generator(
+            prompt=prompt,
+            init_image=init_image,
+            num_images=num_image,
+            num_inference_steps=num_inference_steps,
+            strength=strength,
+            guidance_scale=guidance_scale,
+            seed=seed,
+        )
+        logger.info(f"NSFW list = {nsfw_content_detected}")
+        return images
+
+    def build(self):
+        prompt = gr.Textbox(label="prompt")
+        init_image = gr.Paint(label="Initial image", type="pil")
+        with gr.Row():
+            seed = gr.Number(0, label="seed : 0 = no seed", precision=0)
+            guidance_scale = gr.Number(7.5, label="guidance_scale")
+        num_inference_steps = gr.Slider(
+            minimum=5, maximum=200, value=50, step=5, label="num_inference_steps"
+        )
+        strength = gr.Slider(
+            minimum=0.0, maximum=1.0, value=0.8, step=0.05, label="prompt strength"
+        )
+        num_images = gr.Slider(
+            minimum=1,
+            maximum=self.max_num_images,
+            value=default.NUM_IMAGES,
+            step=1,
+            label="Number of images",
+        )
+
+        generate_btn = gr.Button("Generate !")
+
+        inputs = [
+            prompt,
+            init_image,
+            num_images,
+            seed,
+            guidance_scale,
+            num_inference_steps,
+            strength,
+        ]
+
+        gallery = gr.Gallery(
+            label="Generated images", show_label=False, elem_id="gallery"
+        ).style(grid=self.gallery_grid, height="auto")
+        outputs = gallery
+
+        generate_btn.click(fn=self, inputs=inputs, outputs=outputs)
+
+
 class ImageInPaintingBuilder:
     def __init__(
         self,
@@ -458,6 +555,11 @@ def main(share, faster, nsfw):
         max_num_images=MAX_NUM_IMAGES,
         images_per_row=IMAGES_PER_ROW,
     )
+    SKETCH_TO_IMAGE_APP_BUILDER = SketchToImageBuilder.from_factory(
+        FACTORY,
+        max_num_images=MAX_NUM_IMAGES,
+        images_per_row=IMAGES_PER_ROW,
+    )
     IMAGE_INPAINTING_APP_BUILDER = ImageInPaintingBuilder.from_factory(
         FACTORY,
         max_num_images=MAX_NUM_IMAGES,
@@ -475,6 +577,8 @@ def main(share, faster, nsfw):
             TEXT_TO_IMAGE_APP_BUILDER.build()
         with gr.Tab("Image to image pipeline"):
             IMAGE_TO_IMAGE_APP_BUILDER.build()
+        with gr.Tab("Sketch to image pipeline"):
+            SKETCH_TO_IMAGE_APP_BUILDER.build()
         with gr.Tab("Image inpainting pipeline"):
             IMAGE_INPAINTING_APP_BUILDER.build()
         with gr.Tab("Image inpainting inplace pipeline"):
